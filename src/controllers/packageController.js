8fe653940910296571597cd48a67c4c418da519a
amit <amit@amit-ka-MacBook-Pro.local>
2025-03-30 10:17:03 -0700

    Added 's'  in controller folder

--- src/controllers/packageController.js
+++ src/controllers/packageController.js
@@ -5,151 +5,99 @@ const path = require('path');
 const Package = require('../models/packageModel')
 const APIFeatures = require('../utils/APIFeatures');
 const slugify = require('slugify');
+const catchAsync = require('../utils/catchAsync');
 
+exports.getAllPackages = catchAsync(async (req, res,next) => {
 
-exports.getAllPackages = async (req, res) => {
-
-    try {
         const features = new APIFeatures(Package.find(), req.query).filter().sort().limitFields().paginate()
         const allPackage = await features.query;
         res.status(200).json({
             status: 200,
             data: allPackage
         })
+});
 
-    } catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: err
-        });
-    }
-};
+exports.getPackage = catchAsync(async (req, res,next) => {
+    const package = await Package.findById(req.params.id)
+    res.status(200).json({
+        status: 200,
+        data: package
+    })
+});
 
-exports.getPackage = async (req, res) => {
-    try {
-        const package = await Package.findById(req.params.id)
-        res.status(200).json({
-            status: 200,
-            data: package
-        })
+exports.createPackage = catchAsync(async (req, res, next) => {
+    const newPackage = await Package.create(req.body)
+    res.status(201).json({
+        status: 'success',
+        data: {
+            newPackage
+        }
+    });
+})
 
-    } catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: err
-        });
-    }
-}
+exports.deletePackage = catchAsync(async (req, res, next) => {
+    await Package.findByIdAndUpdate(req.params.id, { "availability": false })
+    res.status(204).json({
+        status: 'success',
+        data: "Done"
+    });
+})
 
-exports.createPackage = async (req, res) => {
-    try {
-        const newPackage = await Package.create(req.body)
-        res.status(201).json({
-            status: 'success',
-            data: {
-                newPackage
+exports.updatePackage =catchAsync( async (req, res, next) => {
+    const newPackage = await Package.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    })
+    res.status(200).json({
+        status: 'success',
+        data: {
+            newPackage
+        }
+    });
+
+});
+
+exports.getPackageStats = catchAsync(async (req, res,next) => {
+    const packages = await Package.find({ ratingsAverage: { $gte: 1.2 } });
+    const packageStats = await Package.aggregate([
+        {
+            $match: { ratingsAverage: { $gte: 0 } }
+        },
+        {
+            $group: {
+                _id: '$ratingsAverage',
+                avgRating: { $avg: '$ratingsAverage' },
+                numPackage: { $sum: 1 },
+                avgAdultPrice: { $avg: "$price.adult" },
+                avgChildPrice: { $avg: "$price.child" },
+                minAdultPrice: { $min: "$price.adult" },
+                maxAdultPrice: { $max: "$price.adult" },
+                minChildPrice: { $min: "$price.child" },
+                maxChildPrice: { $max: "$price.child" }
             }
-        });
-    } catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: err
-        });
-    }
-}
-
-exports.deletePackage = async (req, res) => {
-    try {
-        await Package.findByIdAndUpdate(req.params.id, { "availability": false })
-        res.status(204).json({
-            status: 'success',
-            data: "Done"
-        });
-    } catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: err
-        });
-    }
-
-}
-exports.updatePackage = async (req, res) => {
-    try {
-        const newPackage = await Package.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true
-        })
-        res.status(200).json({
-            status: 'success',
-            data: {
-                newPackage
+        }, {
+            $sort: {
+                avgAdultPrice: 1
             }
-        });
-    } catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: err
-        });
-    }
+        }
+    ]);
 
-}
+    res.status(200).json({
+        status: 'success',
+        data: {
+            packageStats
+        }
+    });
+});
 
-exports.getPackageStats = async (req, res) => {
-    try {
-        const packages = await Package.find({ ratingsAverage: { $gte: 1.2 } });
-        const packageStats = await Package.aggregate([
-            {
-                $match: { ratingsAverage: { $gte: 0 } }
-            },
-            {
-                $group: {
-                    _id: '$ratingsAverage',
-                    avgRating: { $avg: '$ratingsAverage' },
-                    numPackage: { $sum: 1 },
-                    avgAdultPrice: { $avg: "$price.adult" },
-                    avgChildPrice: { $avg: "$price.child" },
-                    minAdultPrice: { $min: "$price.adult" },
-                    maxAdultPrice: { $max: "$price.adult" },
-                    minChildPrice: { $min: "$price.child" },
-                    maxChildPrice: { $max: "$price.child" }
-                }
-            }, {
-                $sort: {
-                    avgAdultPrice: 1
-                }
-            }
-        ]);
+exports.getMonthlyPlan =catchAsync( async (req, res,next) => {
 
-        res.status(200).json({
-            status: 'success',
-            data: {
-                packageStats
-            }
-        });
-    } catch (error) {
-        res.status(400).json({
-            status: 'fail',
-            message: error.message
-        });
-    }
-};
-
-exports.getMonthlyPlan = async (req, res) => {
-
-    try {
-        const year = req.params.year * 1;
-        const plan = await Package.aggregate([]);
-        res.status(200).json({
-            status: 'success',
-            data: {
-                plan
-            }
-        });
-
-    } catch {
-        res.status(400).json({
-            status: 'fail',
-            message: error.message
-        });
-    }
-}
+    const year = req.params.year * 1;
+    const plan = await Package.aggregate([]);
+    res.status(200).json({
+        status: 'success',
+        data: {
+            plan
+        }
+    });
+});
