9fa634ebb0a19775a3938a6ea7172198ec36ff2e
amit <amit@amit-ka-MacBook-Pro.local>
2025-03-30 09:40:28 -0700

    Error middle ware

--- src/app.js
+++ src/app.js
@@ -13,12 +13,21 @@ app.use(express.json());
 
 app.use('/api/v1/packages',packageRouter)
 app.use('/api/v1/users',userRouter)
-app.all('*', (req, res) => {
 
-    res.status(404).json({
-        status: 'fail',
-        message:   `Can't find ${req.originalUrl} on this server!`   
-    })
+app.all('*', (req, res,next) => {
+    const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+    err.statusCode = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+
+    res.status(err.statusCode || 500).json({
+        status: 'error',
+        message: err.message || 'Something went wrong!'   
+     });
+
+    
 });
 
 module.exports = app;
