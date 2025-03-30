d5fe05d4e3b274a390d94cdff4c296744ed290f4
amit <amit@amit-ka-MacBook-Pro.local>
2025-03-30 12:53:05 -0700

    Forment error in error controller

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -1,10 +1,41 @@
 
+const sendErrorDev =(err,res)=>{
+    res.status(err.statusCode || 500).json({
+        status: err.status || 'error',
+        message: err.message || 'Something went wrong!',
+        stack: err.stack,
+        error: err
+    });
+}
+
+const sendErrorProd =(err,res)=>{
+    if(err.isOperational){
+        res.status(err.statusCode || 500).json({
+            status: err.status || 'error',
+            message: err.message || 'Something went wrong!',
+        });
+    }else{
+        // log the error
+        console.error('Error!!!', err.message);
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went wrong!',
+        });
+    }
+
+}
+
 
 module.exports = (err, req, res, next) => {
-    res.status(err.statusCode || 500).json({
-        status: 'error',
-        message: err.message || 'Something went wrong!'
-    });
+
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, res);
+
+    }
+    else if (process.env.NODE_ENV === 'production') {
+        sendErrorProd(err, res);
+    }
+
 
 
 }
