b6f2aa5703ce71b3070219c10f2895000ed7b901
amit <amit@amit-ka-MacBook-Pro.local>
2025-04-01 13:07:05 -0700

    Implemented user routes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,14 +1,15 @@
 
 const User = require('../models/userModal');
 const APIFeatures = require('../utils/APIFeatures');
-exports.getAllUser = async (req,res) => {
+const catchAsync = require('../utils/catchAsync');
+exports.getAllUser = catchAsync(async (req,res) => {
     const features = new APIFeatures(User.find(), req.query).filter().sort().limitFields().paginate();
     const allUser = await features.query;
     res.status(200).json({
         status: 200,
         data: allUser
     })
-}
+});
 
 exports.createUser =(req,res) => {
     res.status(500).json({
