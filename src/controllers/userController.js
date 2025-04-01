393c14c9c4f0166cf00227a239913e6134b21b4d
amit <amit@amit-ka-MacBook-Pro.local>
2025-04-01 13:06:16 -0700

    Implemented user routes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,15 @@
 
-exports.getAllUser =(req,res) => {
-    res.status(500).json({
-        status:'error',
-        message:'This route is not yet defined'
+const User = require('../models/userModal');
+const APIFeatures = require('../utils/APIFeatures');
+exports.getAllUser = async (req,res) => {
+    const features = new APIFeatures(User.find(), req.query).filter().sort().limitFields().paginate();
+    const allUser = await features.query;
+    res.status(200).json({
+        status: 200,
+        data: allUser
     })
 }
+
 exports.createUser =(req,res) => {
     res.status(500).json({
         status:'error',
