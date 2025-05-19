f93b68bd30ac5a9850b173845d458eca44df2756
amit <amit00978@gmail.com>
2025-05-19 11:10:41 -0700

    permision added

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,20 +6,20 @@ const AppError = require('../utils/AppError');
 const fs = require('fs');
 
 
-router.post('/signup', authController.signup);
+router.post('/signup',authController.protect,authController.restrictTo('admin'), authController.signup);
 router.post('/login', authController.login);
-router.post('/forgotPassword', authController.forgotPassword);
-router.post('/changePassword', authController.protect,authController.updatePassword);
-router.put('/resetPassword/:token', authController.resetPassword);
-router.patch('/updateMe',authController.protect,userController.updateMe)
-router.delete('/deleteMe',authController.protect,userController.deleteMe)
+router.post('/forgotPassword',authController.protect,authController.restrictTo('admin'),authController.forgotPassword);
+router.post('/changePassword', authController.protect,authController.restrictTo('admin'),authController.protect,authController.updatePassword);
+router.put('/resetPassword/:token',authController.protect,authController.restrictTo('admin'), authController.resetPassword);
+router.patch('/updateMe',authController.protect,authController.restrictTo('admin'),authController.protect,userController.updateMe)
+router.delete('/deleteMe',authController.protect,authController.restrictTo('admin'),authController.protect,userController.deleteMe)
 
 router.route('/')
-    .get(userController.getAllUser)
-    .post(userController.createUser);
+    .get(authController.protect,authController.restrictTo('admin'),userController.getAllUser)
+    .post(authController.protect,authController.restrictTo('admin'),userController.createUser);
 router.route('/:id')
-    .get(userController.getUser)
-    .patch(userController.updateUser)
+    .get(authController.protect,authController.restrictTo('admin'),userController.getUser)
+    .patch(authController.protect,authController.restrictTo('admin'),userController.updateUser)
     .delete(authController.protect, authController.restrictTo('admin'),userController.deleteUser);
     
 
